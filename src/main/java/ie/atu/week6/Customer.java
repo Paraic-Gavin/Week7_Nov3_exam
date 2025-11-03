ae244b591c6a5afc0471eb4de291b7f1cb22e819
ParaicGavin-STUDENT <G00465477@atu.ie>
2025-11-03 10:20:47 +0000

    exam

--- src/main/java/ie/atu/week6/Customer.java
+++ src/main/java/ie/atu/week6/Customer.java
@@ -1,5 +1,32 @@
 package ie.atu.week6;
+import java.util.ArrayList;
+
 
 public class Customer {
     public static void main(String[]args)
+    private int CustomerId;
+    private String name;
+    private String email;
+    private String Number;
+
+    public Customer(int customerId, String name, String email, String Number) {}
 }
+public int getCustomerId(){
+    return customerId;
+}
+public void setCustomerId(int customerId){
+    this.getcustomerId = CustomerId;
+}
+public String getName(){
+    return name;
+}
+public void setName(int name){
+    this.Name = name;
+}
+public String getEmail(){
+    return email;
+}
+public void setEmail(int email){
+    this.Email = email;
+}
+
